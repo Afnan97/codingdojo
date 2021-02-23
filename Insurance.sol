5632e6877f55c0887b62920fd0b3510a861eedce
Afnan_Qedear <38739666+Afnan97@users.noreply.github.com>
2021-02-23 17:02:53 +0300

    Add files via upload

---
+++ Insurance.sol
@@ -0,0 +1,132 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.8.1;
+
+
+
+/**
+
+ * @title Tech Insurance tor
+
+ * @dev complete the functions
+
+ */
+
+contract TechInsurance {
+
+
+
+    
+
+    struct Product {
+
+        uint productId;
+
+        string productName;
+
+        uint price;
+
+        bool offered;
+
+    }
+
+     
+
+    struct Client {
+
+        bool isValid;
+
+        uint time;
+
+    }
+
+
+
+    mapping(uint => Product) public productIndex;
+
+    mapping(address => mapping(uint => Client)) public client;
+
+    
+
+    uint productCounter;
+
+    
+
+    address payable public insOwner ;
+
+    constructor(address payable _insOwner) public{
+
+        insOwner = _insOwner;
+
+    }
+
+ 
+
+    function addProduct(uint _productId, string memory _productName, uint _price ) public {
+
+        Product memory newProduct = Product (_productId, _productName, _price, true);
+
+        productIndex[productCounter++] = newProduct;
+
+    }
+
+    
+
+    
+
+    function changeFalse(uint _productIndex) public onlyOwner {
+
+        productIndex[_productIndex].offered = false;
+
+    }
+
+    
+
+    function changeTrue(uint _productIndex) public onlyOwner{
+
+        productIndex[_productIndex].offered = true;
+
+    }
+
+    
+
+    modifier onlyOwner {
+
+      require(msg.sender == insOwner, "you are not the owner" );
+
+      _;
+
+      revert("do not run this function again it cost you some fees");
+
+     }
+
+ 
+
+     
+
+    function changePrice(uint _productIndex, uint _price) public onlyOwner{
+
+        productIndex[_productIndex].price = _price;
+
+    }
+
+
+
+    function buyInsurance(uint _productIndex) public payable {
+
+        require(productIndex[_productIndex].offered == true, "Tha Insurance is not available");
+
+        Client memory newClient = Client (true, block.timestamp);
+
+        client[msg.sender][_productIndex] =   newClient ;
+
+        require(msg.value  == productIndex[_productIndex].price, "check the amount of the Insurance");
+
+    } 
+
+
+
+    
+
+}
+
